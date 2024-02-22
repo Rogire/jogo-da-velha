69f6f06e01262f91dfdff79ca9c12f280e7e30ba
Rogire <igorcorreatrifilioc@gmail.com>
2024-02-22 11:55:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import JDV from "./componentes/JDV";
 import Placar from "./componentes/Placar";
-//import atras from "./componentes/imgs/atras.png"
+import atras from "./componentes/imgs/atras.png"
 export default function App() {
   const [jogador, setJogador] = useState("Bola");
   const [WB, setWBol] = useState(0);
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <>
       <a href="https://rogire.github.io/Repositorio/index.html">
-        <img src="/componentes/imgs/atras.png" alt="voltar" className="img" style={{ width: '50px' }}></img>
+        <img src={atras} alt="voltar" className="img" style={{ width: '50px' }}></img>
       </a>
 
       <div className="Container">
